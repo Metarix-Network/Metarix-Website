1d063b9a781079fae300e5f67ea632623a9d1e80
Ubuntu <ubuntu@ip-172-31-10-60.us-west-1.compute.internal>
2022-05-21 18:27:17 +0000

    21/05/22

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,20 @@ $(document).ready(function(){
 	});
 });
 
+function contractFunction1()
+{
+	var contractAddress="0xab75a21fd3cbf92e5e10c27982562e9b9ebbae2e";
+	var rpcUrl="https://bsc-dataseed.binance.org/";
+	var abi=[{"inputs":[{"internalType":"uint256","name":"_maxCap","type":"uint256"},{"internalType":"uint256","name":"_saleStartTime","type":"uint256"},{"internalType":"uint256","name":"_saleEndTime","type":"uint256"},{"internalType":"address payable","name":"_projectOwner","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"NAME","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"buy","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"investedAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxCap","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"projectOwner","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"saleEndTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"saleStartTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalBnbReceived","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+	const providerSign = new ethers.providers.Web3Provider(window.ethereum);
+	const walletSigner = providerSign.getSigner();
+	const contractData = new ethers.Contract(contractAddress, abi, walletSigner);
+	return contractData;
+}
+
 function progressBar()
 {
-	let contract = contractFunction();
+	let contract = contractFunction1();
 	contract.totalBnbReceived().then((data1) => {
 		contract.maxCap().then((data2) => {
 			var result=(((data1)/(data2))*100).toFixed(2);
